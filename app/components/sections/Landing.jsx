44676a830e1b9c175ed55bb1085a011499a12239
shalom99 <castroshalom77@gmail.com>
2023-05-22 19:18:18 +0800

    animation fix trial 2

--- app/components/sections/Landing.jsx
+++ app/components/sections/Landing.jsx
@@ -1,9 +1,10 @@
 "use client";
-import { motion } from "framer-motion";
+
 import Link from "next/link";
 import { RiUserStarLine } from "react-icons/ri";
 import { GiOnTarget } from "react-icons/gi";
 import { BsAward } from "react-icons/bs";
+import { Fade } from "react-awesome-reveal";
 
 export default function Hero() {
   return (
@@ -12,7 +13,9 @@ export default function Hero() {
 
         {/* left */}
         <div className="md:w-[70%] h-full md:pl-[10%] flex flex-col justify-center gap-2 md:gap-3">
+          <Fade>
           <p className="text-center text-xl md:text-start font-bold text-accent">WELCOME TO PRO-LAB</p>
+          </Fade>
           <h2 className="text-center text-3xl font-bold md:text-start lg:text-5xl xl:text-6xl 2xl:text-7xl lg:w-[700px]">Accurate results for healthier lives</h2>
           <h3 className="lg:w-[600px] text-slate-600 text-center lg:text-start">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
